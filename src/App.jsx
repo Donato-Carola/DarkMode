ddcc19f10aae9912697d0f52e2bdee66d5443c8a
Donato Carola <don94@live.it>
2024-12-03 15:57:07 +0100

    style button and card

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Cards from "./components/Cards";
-
+import Cards from "./components/Cards/Cards";
+import ChangeButton from "./components/ChangeButton/ChangeButton";
 
 
 function App() {
   return <>
+  <ChangeButton/>
    <Cards/>
 
 
---
+++ src/components/ChangeButton/ChangeButton.jsx
@@ -0,0 +1,17 @@
+
+
+const ChangeButton = () => {
+  return (
+    <>
+    <div className="w-100 flex items-center justify-center">
+      <button className="btn mb-4 mt-4 uppercase bg-cyan-500"> Cambia </button>
+      </div>
+      
+    </>
+  )
+}
+
+export default ChangeButton
+
+
+
